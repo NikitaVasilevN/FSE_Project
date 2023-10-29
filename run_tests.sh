f1f4ef17e9139d8d1f6d1282ba92595d8670d902
Your Name <you@example.com>
2023-10-29 16:39:55 +0300

    added comments

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 function run_test() {
     test_name=$1
@@ -7,30 +6,29 @@ function run_test() {
 
     echo -n "$test_name: "
 
-    # Запускаем программу и передаем ввод
+    # Running program and giving input
     echo -e "$input" | ./main
 
-    # Проверяем вывод программы
+    # Checking program output
     if grep -q "$expected_output" results.txt; then
         echo "PASSED"
     else
         echo "FAILED"
         echo "Expected '$expected_output' in results.txt, but it was not found"
-        cat results.txt  # Показать содержимое файла results.txt
+        cat results.txt  # Show results.txt
     fi
 
-    # Очищаем результаты перед следующим тестом
+    # Cleaning the results before next test
     rm -f results.txt
 }
 
-# Компилируем программу
+# Compiling program
 g++ -o main main.cpp
 
-# Запускаем тесты
+# Running tests
 run_test "Test 1: Normal input" "3\n5\n10\n25\n" "GCD of the list: 5"
 run_test "Test 2: All same numbers" "3\n7\n7\n7\n" "GCD of the list: 7"
 run_test "Test 3: Negative numbers" "3\n-9\n-18\n-27\n" "GCD of the list: 9"
 run_test "Test 4: Mixed positive and negative" "3\n15\n-30\n45\n" "GCD of the list: 15"
 run_test "Test 5: Including zero" "3\n0\n20\n40\n" "GCD of the list: 20"
 
-# И так далее для других функций и тестовых случаев
