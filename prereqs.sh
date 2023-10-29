4178f81ef2bb2abacb21bdace1d9ba1170bff362
Dmitry Nalbersky <almamatr2141@gmail.com>
2023-10-29 18:43:09 +0300

    Decrease image size

--- prereqs.sh
+++ prereqs.sh
@@ -5,3 +5,6 @@ apt install cmake g++ unzip wget -y
 mkdir opencv && cd opencv
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
 unzip opencv.zip && mkdir -p build && cd build && cmake ../opencv-4.x -DBUILD_LIST=imgproc,imgcodecs && make -j 3
+cd ..
+rm opencv.zip
+rm -r opencv-4.x
