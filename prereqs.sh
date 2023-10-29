e611c8c1111481980afc2e5b63f5957b661d16e8
rows-of-houses <117982412+rows-of-houses@users.noreply.github.com>
2023-10-30 00:59:25 +0300

    Update prereqs.sh

--- prereqs.sh
+++ prereqs.sh
@@ -6,5 +6,10 @@ mkdir opencv && cd opencv
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
 unzip opencv.zip && mkdir -p build && cd build && cmake -DBUILD_LIST=imgproc,imgcodecs ../opencv-4.x && make -j 3
 export OpenCV_DIR=$(pwd)
+echo $OpenCV_DIR
+echo $OpenCV_DIR
+echo $OpenCV_DIR
+echo $OpenCV_DIR
+echo $OpenCV_DIR
 cd ..
 rm opencv.zip
