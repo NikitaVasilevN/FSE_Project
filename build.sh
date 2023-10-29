0b6d7f83b0e28a9b76e37025be2e745832fe70cf
rows-of-houses <117982412+rows-of-houses@users.noreply.github.com>
2023-10-30 02:12:50 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export OpenCV_DIR=$(cat opencv_dir_keeper)
-echo "I'm in source"
+echo "I'm in build"
 echo $OpenCV_DIR
-echo "I'm in source"
+echo "I'm in build"
 echo "2"
 echo "2"
 echo "2"
 mkdir -p build && cd build && cmake .. && make
+cd ..
